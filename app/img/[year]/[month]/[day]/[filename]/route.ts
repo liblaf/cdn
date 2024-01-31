07b9deb030301832c9d1d277c1f89d34489eb4e1
liblaf <30631553+liblaf@users.noreply.github.com>
2024-01-31 15:50:50 +0800

    feat: update image route to simplify URL structure
    Simplify the URL structure for image routes by removing the unnecessary month and day segments. This change improves the readability and maintainability of the image URLs.

--- app/img/[year]/[month]/[day]/[filename]/route.ts
+++ app/img/[year]/[month]/[day]/[filename]/route.ts
@@ -2,16 +2,21 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const runtime = "edge";
 
+type Params = {
+  year: string;
+  month: string;
+  day: string;
+  filename: string;
+};
+
 export async function GET(
   request: NextRequest,
-  {
-    params,
-  }: { params: { year: string; month: string; day: string; filename: string } },
+  { params }: { params: Params },
 ): Promise<NextResponse> {
   return new NextResponse(undefined, {
     status: 302,
     headers: {
-      Location: `https://alist.liblaf.me/d/public/img/${params.year}/${params.year}-${params.month}-${params.day}T${params.filename}`,
+      Location: `https://alist.liblaf.me/d/public/img/${params.year}/${params.filename}`,
     },
   });
 }
